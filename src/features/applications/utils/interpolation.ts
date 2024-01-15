effd6d9927246cd33b8a937a14409f614a98d0e2
CustomEntity <customentity@gmail.com>
2024-01-15 15:24:08 +0100

    Fix kafka

--- src/features/applications/utils/interpolation.ts
+++ src/features/applications/utils/interpolation.ts
@@ -8,5 +8,8 @@ export function interpolateString(
   string: string,
   data: Record<string, string>,
 ): string {
+  if (!string) {
+    return '';
+  }
   return string.replace(/{{(.*?)}}/g, (_, g) => data[g] ?? '');
 }
