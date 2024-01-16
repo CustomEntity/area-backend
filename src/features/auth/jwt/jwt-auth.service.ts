9efea5ddafeb2cece13f1027a87de29a7d53ed85
CustomEntity <customentity@gmail.com>
2024-01-16 18:17:33 +0100

    changed cookie security

--- src/features/auth/jwt/jwt-auth.service.ts
+++ src/features/auth/jwt/jwt-auth.service.ts
@@ -24,7 +24,6 @@ export class JwtAuthService {
     res.cookie('access_token', jwt, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      domain: process.env.COOKIE_DOMAIN ?? 'localhost',
     });
 
     return {
