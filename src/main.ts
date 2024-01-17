3962013184df6e4a92a0c4834930245ef4867b2b
CustomEntity <customentity@gmail.com>
2024-01-17 01:08:37 +0100

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -19,15 +19,16 @@ let server: { close: (arg0: (err: any) => void) => void };
 BigInt.prototype.toJSON = function (): string {
   return this.toString();
 };
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-        'http://localhost:3000',
-        'http://localhost:5173',
-        'tauri://localhost',
-        'https://lfdp.eu',
-        'http://lfdp.eu',
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'tauri://localhost',
+      'https://lfdp.eu',
+      'http://lfdp.eu',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
